09fa4242853143e1caf155e2a2b8c86d08dd47ab
Macbook <macbook@MacBook-Air-de-Macbook.local>
2022-07-22 18:52:14 +0200

    welcome message

--- srcs/command/command.cpp
+++ srcs/command/command.cpp
@@ -8,25 +8,25 @@ Command::Command()
 Command::Command(std::string message) : _recv(message)
 {
 	std::vector<std::string> tmp;
-	std::cout << message << std::endl;
+	// std::cout << "message " << message << std::endl;
 	tmp = split(message, ":");
 	_parameters = split(tmp[0], " ");
 	_prefix = _parameters[0];
 	_parameters.erase(_parameters.begin());
-	for (std::vector<std::string>::iterator it = _parameters.begin(); it != _parameters.end(); it++)
-	{
-		std::cout << "parameters : " << *it << std::endl;
-	}
+	// for (std::vector<std::string>::iterator it = _parameters.begin(); it != _parameters.end(); it++)
+	// {
+	// 	std::cout << "parameters : " << *it << std::endl;
+	// }
 	if (tmp.size() > 1)
 	{
 		_message = tmp[1];
-		std::cout << "message : " << _message << std::endl;
+		// std::cout << "message : " << _message << std::endl;
 	}
 	for (size_t i = 0; i < _prefix.length(); i++)
 	{
 		_prefix[i] = std::toupper(_prefix[i]);
 	}
-	std::cout << "prefix : " << _prefix << std::endl;
+	// std::cout << "prefix : " << _prefix << std::endl;
 }
 
 Command::~Command()
--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -49,52 +49,77 @@ void Server::init()
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(6667);
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	if (fcntl(socketfd, F_SETFL, O_NONBLOCK) <-1)
+	if (fcntl(socketfd, F_SETFL, O_NONBLOCK) < -1)
 		strerror(errno);
 	if (bind(socketfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) == -1)
 		strerror(errno);	
-	if (listen(socketfd, 3) == -1)
+	if (listen(socketfd, NB_CLIENTS_MAX) == -1)
 		return ;
 	addr_size = sizeof(addr);
 	_fds.fd = socketfd;
 	_fds.events = POLLIN;
-	std::cout << "POLLIN" << POLLIN << std::endl;
-	std::cout << "POLLHUP" << POLLHUP << std::endl;
+	std::cout << "Welcome to IRCinator" << std::endl;
 }
 
 void Server::launch()
 {
 
 	std::vector<struct pollfd>		pollfds;
-	struct pollfd					client;
+	Client 							*client;
 
 	for (;;)
 	{
 		pollfds.push_back(_fds);
-		if (poll(&pollfds[0], pollfds.size() + 1, -1) == -1)
+		if (poll(&pollfds[0], _nbClients + 1, -1) == -1)
 			strerror(errno);
 		for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); it++)
 		{
+			std::cout << "client tab fd : " << (*it).second->getPoll().fd << std::endl;
 			pollfds.push_back((*it).second->getPoll());
 		}
-		std::vector<pollfd>::iterator beg = pollfds.begin();
-		std::vector<pollfd>::iterator end = pollfds.end();
-		while (beg != end)
+		if (pollfds[0].revents & POLLIN)
 		{
-			if (beg->revents & POLLIN)
+			std::cout << "server :" << std::endl;
+			if (_nbClients < NB_CLIENTS_MAX )
+				acceptClient();
+			std::cout << "nb client : " << _nbClients << std::endl;
+			std::cout << "fd : " << pollfds[0].fd << std::endl;
+		}
+		else {
+			std::vector<pollfd>::iterator beg = pollfds.begin();
+			std::vector<pollfd>::iterator end = pollfds.end();
+			while (beg != end)
 			{
-				if (beg->fd == _fds.fd)
+				// if (beg->revents & POLLHUP && beg->fd != _fds.fd)
+				// {
+				// 	removeClient(beg->fd);
+				// }
+				if (beg->revents & POLLIN)
 				{
-					std::cout << "server :" << std::endl;
-					acceptClient();
-					std::cout << "nb client : " << pollfds.size() << std::endl;
+					// for (std::map<int, Client *>::iterator it = _clients.begin(); it != _clients.end(); it++)
+					// {
+					// 	std::cout << "client status " << it->second->getStatus() << std::endl;
+					// }
+					client = _clients[beg->fd];
+					if (client == NULL)
+						return ;
+					client->treatMessage();
+					// std::cout << "status 1:" << client->getStatus() << std::endl;
+					if (client->getStatus() == DISCONNECTED)
+						removeClient(client->getPoll().fd);
+					else if (client->getStatus() == PENDING)
+						rplWelcome(client);
+					else if (client->getStatus() == CONNECTED)
+						// std::cout << "status :" << client->getStatus() << std::endl;
+					client->clearCommands();
+					std::cout << "fd : " << client->getPoll().fd << std::endl;
+
 				}
-				else {
-					client = _clients[beg->fd]->getPoll(); 
-					_clients[beg->fd]->treatMessage();
-				}
+					// else{
+					// 	std::cout << "else" << std::endl;
+					// }
+				beg++;
 			}
-			beg++;
 		}
 		pollfds.clear();
 	}
@@ -116,14 +141,16 @@ void Server::acceptClient()
 		
 	fds.fd = client_sock;
 	fds.events = POLLIN;
+	fds.revents = POLLIN;
 	this->_clients.insert(std::pair<int, Client *>(client_sock, new Client(fds, static_cast<Server &>(*this))));
-	this->_nb_clients += 1;
+	this->_nbClients += 1;
 }
 
 void Server::removeClient(int fd)
 {
+	close(fd);
 	this->_clients.erase(fd);
-	this->_nb_clients -= 1;
+	this->_nbClients -= 1;
 }
 
 bool Server::searchNickname(std::string nickname)
@@ -136,7 +163,25 @@ bool Server::searchNickname(std::string nickname)
 	return false;
 }
 
-// void Server::rplWelcome(Client client)
-// {
-// 	return ;
-// }
+void Server::rplWelcome(Client *client)
+{
+	std::string reply;
+	std::string reply2;
+	std::cout << "********REGISTRATION SUCCESS for " << client->getNickname() << "**********" << std::endl;		
+	std::cout << "fd registred : " << client->getPoll().fd << std::endl;
+
+	client->setStatus(CONNECTED);
+	// _nbClients++;
+	reply += ":" + _name + " 001 " + client->getNickname() + " :Welcome to the Internet Relay Network " + client->getNickname() + "\r\n";
+	reply2 +=  ":" + _name + " 376 " + client->getNickname() + " :End of /MOTD command\r\n";
+	send(client->getPoll().fd, reply.c_str(), reply.size(), 0);
+	send(client->getPoll().fd, reply2.c_str(), reply2.size(), 0);
+}
+
+void Server::changeClientClass(Client oldClient, Client *newClient)
+{
+	t_pollfd newPoll;
+	newPoll = oldClient.getPoll();
+	newClient->setPoll(newPoll);
+	_clients[oldClient.getPoll().fd] = newClient; 
+}
